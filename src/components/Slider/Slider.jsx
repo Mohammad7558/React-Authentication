54651a003e62d208ccb81a9b2e4ea4f932d88b5c
Mohammad <108111892+Mohammad7558@users.noreply.github.com>
2025-05-07 17:18:03 +0600

    added-light-weight-imgforSlider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation, EffectFade } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import "swiper/css/effect-fade";
 
-import banner1 from "../../assets/banner-1-min.jpg";
-import banner2 from "../../assets/banner-2-min.jpg";
-import banner3 from "../../assets/banner-3-min.jpg";
+import banner1 from "../../assets/banner-1-min-min.jpg";
+import banner2 from "../../assets/banner-2-min-min.jpg";
+import banner3 from "../../assets/banner-3-min-min.jpg";
 
 const slides = [
   {
@@ -32,22 +31,25 @@ const HeroSlider = () => {
   return (
     <section className="relative">
       <Swiper
-        modules={[Autoplay, Pagination, Navigation, EffectFade]}
-        effect="fade"
+        modules={[Autoplay, Pagination, Navigation]}
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{ delay: 4000, disableOnInteraction: false }}
+        speed={800}
         pagination={{ clickable: true }}
         navigation={true}
         className="w-full h-[90vh]"
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index}>
-            <div
-              className="w-full h-full bg-cover bg-center relative"
-              style={{ backgroundImage: `url(${slide.image})` }}
-            >
-              <div className="absolute inset-0 bg-[#0000007b] bg-opacity-20 flex items-center justify-center">
+            <div className="w-full h-full relative">
+              <img
+                src={slide.image}
+                alt={slide.title}
+                loading="lazy"
+                className="w-full h-full object-cover absolute top-0 left-0"
+              />
+              <div className="absolute inset-0 bg-black/50 flex items-center justify-center will-change-transform">
                 <div className="text-center px-4 text-white max-w-2xl">
                   <h2 className="text-4xl md:text-5xl font-bold mb-4 drop-shadow-md">
                     {slide.title}
